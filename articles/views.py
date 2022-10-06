27e9d4edeaaa784061b3087f117db4f52a2ff315
murdex5 <fkadupiige@gmail.com>
2022-10-06 13:09:27 +0200

    added new html and folders and added new view to the  views folder

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Article
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse(request, 'Hello World')
+
+def home(request):
+    context = {}
+    
+    return render(request, 'home.html', context=context)
