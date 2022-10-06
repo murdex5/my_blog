425313f73ec8c24c2ae0e49959f5c3cae7e0dbad
murdex5 <fkadupiige@gmail.com>
2022-10-06 12:48:37 +0200

    Added a model class class called Article

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+class Articles(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length = 100)
+    body = models.TextField()
+    
+    def __str__(self):
+        return self.title
