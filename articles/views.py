25cd326daf02e4c6f0a814f3c91e61d210998108
murdex5 <fkadupiige@gmail.com>
2022-10-12 11:37:10 +0200

    views.py is changed to the basics

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,20 @@
+import re
+from urllib.request import Request
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
+def home(request):
+    return HttpResponse('home')
+
+
+def catalog(request):
+    return HttpResponse('catalog')
+
+
+def create(request):
+    return HttpResponse('create')
+
+def article_view(request):
+    return HttpResponse('article-view')
