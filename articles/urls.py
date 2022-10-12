aa8f992279c394467935ae4255941ba1708a4964
murdex5 <fkadupiige@gmail.com>
2022-10-12 11:34:08 +0200

    everything is cleared to start from the begining

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+
+# class import 
+
+# from .views import Articles
+
 urlpatterns = [
-    path('', views.home, name='index'),
-    path('articles/<int:pk>', views.article, name='articles'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,30 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.core import serializers
 
-from .models import Article
 # Create your views here.
 
-
-
-def home(request):
-    articles = serializers.serialize( "python", Article.objects.all() )
-    context = {
-        'articles': articles,
-    }
-    
-    return render(request, 'home.html', context=context)
-
-
-def article(request, pk):
-    pk = Article.objects.get(id=pk)
-    name = Article.title()
-    body = Article.body()
-    author = Article.author()
-    
-    context = {
-        'name': name,
-        'body': body,
-        'author': author,
-    }
-    return render(request, 'article-view.html', context=context)
